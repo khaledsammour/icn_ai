77bbed1cc5cbfd3b01d642a0b3ed171e75f6c691
khaled taplane <khaled@taplane.com>
2024-07-17 11:39:11 +0300

    palestinian

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -5,7 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path, re_path
 from apps.home import views
-from .views import ScrapView, SecScrapView, GameakScrapView
+from .views import ScrapView, SecScrapView, GameakScrapView, PalestinianScrapView
 
 urlpatterns = [
 
@@ -17,4 +17,5 @@ urlpatterns = [
     path('api/scrap', ScrapView.as_view()),
     path('api/SecScrapView', SecScrapView.as_view()),
     path('api/GameakScrapView', GameakScrapView.as_view()),
+    path('api/PalestinianScrapView', PalestinianScrapView.as_view()),
 ]
